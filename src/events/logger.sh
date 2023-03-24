e553e63d455668ab3d13c2b0174b64ecf256b165
nHexanol <72498523+nHexanol@users.noreply.github.com>
2023-03-24 20:21:29 +0700

    Delete logger.sh

--- src/events/logger.sh
+++
@@ -1,7 +0,0 @@
-curl -v \
--H "Authorization: Bot OTIxODY4MjU4NTAyNjY4MzE4.GES1o6.zFg-_BvxeHHqbB0FGfzZV6eC8KsW2g9rWeSBx8" \
--H "User-Agent: curl/7.71.0" \
--H "Content-Type: application/json" \
--X POST \
--d "{\"content\":\"Logged in.\"}" \
-https://discordapp.com/api/channels/1058954802018648114/messages
